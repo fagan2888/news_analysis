25e8fee9c874c5d2d7c0faa36a534d1c14c908d7
Warren <warrenronsiek@gmail.com>
2017-04-06 16:58:18 -0700

    now working with jezebel included

--- news_analysis.py
+++ news_analysis.py
@@ -15,22 +15,25 @@ stopwords = set(stopwords.words('english'))
 
 class NewsAnalysis(object):
     def __init__(self, voxfile, jezebelfile):
-        vox = self.read(voxfile, jezebelfile)
-        vox = self.clean(vox)
-        vox, topic_word_assoc = self.lda(vox)
-        self.vox = vox
+        vox, jezebel = self.read(voxfile, jezebelfile)
+        corpus = self.clean(vox, jezebel)
+        corpus, topic_word_assoc = self.lda(corpus)
+        self.corpus = corpus
         self.topic_word_assoc = topic_word_assoc
 
     @staticmethod
     def read(voxfile, jezebelfile):
         vox = pd.read_json('./data/' + voxfile, lines=True)
         jezebel = pd.read_json('./data/' + jezebelfile, lines=True)
-        return vox
+        return vox, jezebel
 
     @staticmethod
-    def clean(vox):
+    def clean(vox, jezebel):
+        jezebel['id'] = jezebel._id.apply(lambda x: x['$oid'])
+        jezebel.drop('_id', axis=1, inplace=True)
         vox['id'] = vox._id.apply(lambda x: x['$oid'])
         vox.drop('_id', axis=1, inplace=True)
+
         trans = str.maketrans('', '', string.punctuation + '0123456789')
         stemmer = SnowballStemmer('english')
 
@@ -45,12 +48,12 @@ class NewsAnalysis(object):
                 if c1 and c2 and c3 and c4:
                     result.append(stemmer.stem(word.encode('ascii', 'ignore').decode('UTF-8')))
             return result
-
         vox.text = vox.text.apply(lambda x: filter_func(x))
+        jezebel.text = jezebel.text.apply(lambda x: filter_func(x))
+
         vox_date = vox.date.apply(
             lambda x: x[:-1] + 'AM' if x.endswith('a') else x[:-1] + 'PM' if x.endswith('p') else x)
         vox_date = vox_date.apply(lambda x: x[:12] if x.endswith('M') else x)
-
         for row in vox_date.iteritems():
             try:
                 if row[1] == 'NULL':
@@ -59,23 +62,26 @@ class NewsAnalysis(object):
                     vox_date[row[0]] = pd.to_datetime(row[1])
             except:
                 pass
-
         vox.date = vox_date
-        return vox
+
+        vox['org'] = 'vox'
+        jezebel['org'] = 'jezebel'
+
+        return pd.concat([vox, jezebel], ignore_index=True)
 
     @staticmethod
-    def lda(vox):
+    def lda(corpus):
         pipeline = Pipeline([
             ('tf', CountVectorizer()),
             ('lda', LatentDirichletAllocation(learning_method='online', max_iter=30))
         ])
 
         parameters = {
-            'lda__n_topics': range(5, 6)
+            'lda__n_topics': range(5, 15)
         }
 
         gs = GridSearchCV(pipeline, parameters)
-        gs.fit(vox.text.apply(lambda x: ' '.join(x)))
+        gs.fit(corpus.text.apply(lambda x: ' '.join(x)))
 
         topic_word_assoc = []
 
@@ -86,13 +92,15 @@ class NewsAnalysis(object):
         tf_feature_names = gs.best_estimator_.steps[0][1].get_feature_names()
         lda = gs.best_estimator_.steps[1][1]
         get_top_words(lda, tf_feature_names, 10)
-        topic_matrix = gs.transform(vox.text.apply(lambda x: ' '.join(x)))
+        topic_matrix = gs.transform(corpus.text.apply(lambda x: ' '.join(x)))
         topic_df = pd.DataFrame(topic_matrix)
         topic_df.columns = ['topic' + str(t) for t in topic_df.columns]
         topic_df['max_category'] = topic_df.apply(lambda x: x.index[x == np.max(x)][0], axis=1)
-        vox = pd.concat([vox, topic_df], ignore_index=True, axis=1)
-        return vox, topic_word_assoc
+        corpus = pd.concat([corpus, topic_df], ignore_index=True, axis=1)
+        return corpus, topic_word_assoc
 
 
 if __name__ == '__main__':
     na = NewsAnalysis('voxtest.jsonl', 'jezebeltest.jsonl')
+    print(na.corpus.head())
+    print(na.topic_word_assoc)
